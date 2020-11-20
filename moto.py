19f38517b77ef4cb4b70b33ff38c763d2522c58f
Harredando <hdfranne@gmail.com>
2020-11-20 15:17:48 +0100

    From swedish to english

--- moto.py
+++ moto.py
@@ -6,8 +6,8 @@ GPIO.setmode(GPIO.BCM)
 
 #Set up pins for different servos 
 servoPIN1 = 17
-servoPIN2 = 1337 #Här skrivs lämplig pin in
-servoPIN3 = 1337 #Här skrivs lämplig pin in
+#servoPIN2 = 1337 #Choose appropriate PIN
+#servoPIN3 = 1337 #Choose appropriate PIN
 
 
 GPIO.setup(servoPIN1, GPIO.OUT)
@@ -17,22 +17,19 @@ servoMilk = GPIO.PWM(servoPIN2, 50) # GPIO 1337 for PWM with 50Hz
 GPIO.setup(servoPIN2, GPIO.OUT)
 servoPlate = GPIO.PWM(servoPIN3, 50) # GPIO 1337 for PWM with 50Hz
 
-svar = False  ######### Här väljer man sin mjölk mängd som mann vill ha ###########
+svar = False  ######### Choose milk amount ###########
 while svar == False:
 
-  #Tänker att standard mängden blir svag och att man väljer att öka respektive med upp till 10%
   milkAmount = 1
-  milkAmount = int(input("Hur mycket mjölk vill du ha? (Svar måste vara mellan 0 - 10)" ))
+  milkAmount = int(input("How much milk? (Must be between 0 - 10)" ))
   if(milkAmount < 0 or milkAmount > 10):
     print("Svar ej giltigt.")
 
   else: 
     svar = True 
 
-svar2 = False  ######### Här väljer man sin kaffe mängd som mann vill ha ###########
+svar2 = False  ######### Choose coffr amount ###########
 while svar2 == False:
-
-  # Tänker att standard mängd blir 5 och att man väljer att öka respektive sänka med 5% + att 0 exkluderar helt
 
   coffeeAmount = int(input("Hur starkt kaffe vill du ha? (Svar måste vara mellan 0 - 10)" ) ) 
   if(coffeeAmount < 0 or coffeeAmount > 10):
@@ -41,18 +38,18 @@ while svar2 == False:
   else: 
     svar2 = True 
 
-svar3 = False   ######### Här väljer man sin mugg ###########
+svar3 = False   ######### Mug choice ###########
 while svar3 == False:
-  mugNr = int(input("Välj din mugg! (Svar måste vara 1, 2, 3 eller 4)"))   
+  mugNr = int(input("Choose mug! (Has to be 1, 2, 3 eller 4)"))   
   if(mugNr < 1 or mugNr > 4):
     print("Svar ej giltigt.")
 
   else:
     svar3 = True
 
-#### Här tänkte jag fixa så att de första frågorna faktiskt justerar något
+#### Adjustemnt of amounts
 
-#Standardvärde för samtliga saker
+#Standardvalue
 
 stdValueCoffee = 5
 stdValueMilk = 5
@@ -64,14 +61,14 @@ elif (mugNr == 3):plateFinal = 180
 elif (mugNr == 4):plateFinal = 270
 
 
-coffeeFinal = stdValueCoffee * (coffeeAmount / 100) ##Här justeras div grejjor
+coffeeFinal = stdValueCoffee * (coffeeAmount / 100) ##justeras div grejjor
 
-milkFinal = stdValueMilk * (milkAmount / 100)  ##Här justeras div grejjor
+milkFinal = stdValueMilk * (milkAmount / 100)  ##justeras div grejjor
 
 
 
-print("Du valde mjölkmängd: " , milkAmount)
-print("Du valde kaffemängd: " , coffeeAmount)
+print("Du valde milkamount: " , milkAmount)
+print("Du valde coffeamount: " , coffeeAmount)
 print("Du valde mugg: " , mugNr)
 
 
@@ -85,15 +82,15 @@ print("Du valde mugg: " , mugNr)
 #  servoPlate.stop()
 #  GPIO.cleanup()
 
-#servoCoffee.start(2.5) # Initialization
-#try:
-#  while True:
-#    servoCoffee.ChangeDutyCycle(5)
-#    time.sleep(coffeeFinal)   
-#    
-#except KeyboardInterrupt:
-#  servoCoffee.stop()
-#  GPIO.cleanup()
+servoCoffee.start(2.5) # Initialization
+try:
+  while True:
+    servoCoffee.ChangeDutyCycle(5)
+    time.sleep(coffeeFinal)   
+    
+except KeyboardInterrupt:
+  servoCoffee.stop()
+  GPIO.cleanup()
 
 #servoMilk.start(2.5) # Initialization
 #try:
