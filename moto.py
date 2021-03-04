68e724ef2b7135e2fa2b3fe60c9b8f3498b6497c
Harredando <74529118+Harredando@users.noreply.github.com>
2021-03-04 16:43:54 +0100

    Update moto.py
    Sista uppstädning av koden

--- moto.py
+++ moto.py
@@ -9,114 +9,86 @@ GPIO.setmode(GPIO.BCM)
 servoPIN1 = 17
 servoPIN2 = 22 
 servoPIN3 = 16 
-servoPIN4 = 23
+relayPIN4 = 23
 
 GPIO.setup(servoPIN1, GPIO.OUT)
 servoCoffee = GPIO.PWM(servoPIN1, 50) # GPIO 17 for PWM with 50Hz
 GPIO.setup(servoPIN2, GPIO.OUT)
-servoMilk = GPIO.PWM(servoPIN2, 50) # GPIO 1337 for PWM with 50Hz
+servoMilk = GPIO.PWM(servoPIN2, 50) # GPIO 22 for PWM with 50Hz
 GPIO.setup(servoPIN3, GPIO.OUT)
-servoPlate = GPIO.PWM(servoPIN3, 50) # GPIO 1337 for PWM with 50Hz
-GPIO.setup(servoPIN4, GPIO.OUT)
+servoPlate = GPIO.PWM(servoPIN3, 50) # GPIO 16 for PWM with 50Hz
+GPIO.setup(relayPIN4, GPIO.OUT)
 
 svar = False  ######### Choose milk amount ###########
 while svar == False:
 
   milkAmount = int(input("How much milk? (Must be between 0 - 10)" ))
   if(milkAmount < 0 or milkAmount > 10):
-    print("Svar ej giltigt.")
+    print("Input not permitted")
 
   else: 
     svar = True 
 
-svar2 = False  ######### Choose coffr amount ###########
+svar2 = False  ######### Choose coffee amount ###########
 while svar2 == False:
 
-  coffeeAmount = int(input("Coffe strength? (Svar must var mellan 0 - 10)" ) ) 
+  coffeeAmount = int(input("Coffee strength? (Answer must be between 0 - 10)" ) ) 
   if(coffeeAmount < 0 or coffeeAmount > 10):
-    print("Svar ej giltigt.")
+    print("Input not permitted")
 
   else: 
     svar2 = True 
 
 svar3 = False   ######### Mug choice ###########
 while svar3 == False:
-  mugNr = int(input("Choose mug! (Has to be 1, 2, 3 eller 4)"))   
+  mugNr = int(input("Choose a mug! (Has to be 1, 2, 3 eller 4)"))   
   if(mugNr < 1 or mugNr > 4):
-    print("Svar ej giltigt.")
+    print("Input not permitted")
 
   else:
     svar3 = True
 
 #### Adjustemnt of amounts
 
-#Standardvalue
-
-stdValueCoffee = 5
-stdValueMilk = 5
-stdValuePlate = 0
-
-
-coffeloop = True 
-
-print("Du valde milkamount: " , milkAmount)
-print("Du valde coffeamount: " , coffeeAmount)
-print("Du valde mugg: " , mugNr)
+print("You chose milkamount: " , milkAmount)
+print("You chose coffeamount: " , coffeeAmount)
+print("You chose mug-number: " , mugNr)
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
 
 servoPlate.start(0) # Initialization
-#try:
 servoPlate.ChangeDutyCycle(5)
-    #time.sleep(plateFinal)
 time.sleep(3.7*(mugNr-1))
     
-#except KeyboardInterrupt:
 servoPlate.stop()
-  #GPIO.cleanup()
 
 
-GPIO.output(servoPIN4, 1)
-
+GPIO.output(relayPIN4, 1)
 time.sleep(5)
 
-GPIO.output(servoPIN4,0)
-
+GPIO.output(relayPIN4,0)
 time.sleep(2)
 
 
 servoCoffee.start(0) # Initialization
 try:
-#  while coffeloop == True:
   servoCoffee.ChangeDutyCycle(5)
-   # time.sleep(coffeeFinal)
   time.sleep(coffeeAmount)
   servoCoffee.stop()
-#    coffeloop = False
 
 except KeyboardInterrupt:
   servoCoffee.stop()
- # GPIO.cleanup()
 
 servoMilk.start(0) # Initialization
-#try:
-#  while True:
 servoMilk.ChangeDutyCycle(5)
-time.sleep(milkAmount)  # 3,7 for 1/4 lap
+time.sleep(milkAmount)  
 servoMilk.stop()
 
-#except KeyboardInterrupt:
-#  servoMilk.stop()
-#  GPIO.cleanup()
 
 servoPlate.start(0) # Initialization
-#try:
 servoPlate.ChangeDutyCycle(5)
-    #time.sleep(plateFinal)
-time.sleep(3.7*(4-(mugNr-1)))
-    
-#except KeyboardInterrupt:
+time.sleep(3.7*(4-(mugNr-1)))   
 servoPlate.stop()
-  #GPIO.cleanup()
+ 
 
